ded179e82efa9841125880530b8bf45839ed6515
hdy_glb_remote <hdy_glb_remote@163.com>
2020-09-21 19:52:29 +0800

    test conflict

--- src/test/java/com/atguigu/git/TestApple.java
+++ src/test/java/com/atguigu/git/TestApple.java
@@ -2,7 +2,6 @@ package com.atguigu.git;
 
 public class TestApple {
 	public static void main(String[] args) {
-		System.out.println("123");
-		System.out.println("我新提交的");
+		System.out.println("这是开发人员A");
 	}
 }
