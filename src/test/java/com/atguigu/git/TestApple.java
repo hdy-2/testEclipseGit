ce574eebbce87248a31e947b520fa0420dc83551
hdy_glb_remote <hdy_glb_remote@163.com>
2020-09-21 20:50:13 +0800

    aaaaaaa

--- src/test/java/com/atguigu/git/TestApple.java
+++ src/test/java/com/atguigu/git/TestApple.java
@@ -5,6 +5,6 @@ public class TestApple {
 		System.out.println("开发人员A");
 		System.out.println("????????");
 		System.out.println("开发人员B");
-		System.out.println("我在hot_fix分支上做了更改！！！！！！");
+		System.out.println("我在hot_fix分支上做了更改！！！！！！aaa s");
 	}
 }
